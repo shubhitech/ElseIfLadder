6ace3d9493bdc6c772597d88011027f32fd4f1ba
shubhitech <shubhi171197gupta@gmail.com>
2023-09-18 14:41:20 +0530

    First Commit

---
+++ DemosApp/src/in/ineuron/app/DemoApp.java
@@ -0,0 +1,30 @@
+package in.ineuron.app;
+
+public class DemoApp {
+	
+		public static void main(String[] args) {
+			byte a=10;
+			byte b=20;
+			int x,y,z;
+		x=y=z=40;
+		if (a>b) {
+			System.out.println(a-b);
+		}else {
+			System.out.println(a+b);
+		}
+		System.out.println(x+"  "+y+" "+" "+z);
+			
+			
+			
+		    System.out.println(a<=b); 
+			System.out.println(a+b);
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(a*b);
+			
+
+		}
+
+	}
+
+
